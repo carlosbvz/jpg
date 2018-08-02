a0ffc735a64d0d554ab91ecfd10620b4d3dfa6e3
Jose Pablo Granados <josepablo.granados@digitaslbi.com>
2018-08-02 14:32:23 -0600

    Testing Jenkins integration

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('Team me up')
+  res.send('Team me up app')
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
