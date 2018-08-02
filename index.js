c11744fb7c9c85043b5cae5dbe57b786bed1aa9b
Jose Pablo Granados <josepablo.granados@digitaslbi.com>
2018-08-02 07:47:48 -0600

    First project files

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('Team me up')
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
