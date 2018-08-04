deb7d4ec0e565d306cbb31007500093ee089861a
Carlos Benavides V <carlos.benavides@digitaslbi.com>
2018-08-04 07:58:49 -0600

    Routes and index.html added to proyect

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('Team me up app')
-})
+app.use(express.static('app/public'))
+require('./routes/routes')(app)
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
