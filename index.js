146475fd1ae0a0daff3793cd9481c600f02db03f
Carlos Benavides V <carlos.benavides@digitaslbi.com>
2018-08-04 10:58:56 -0600

    core folder created

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('app/public'))
-require('./routes/routes')(app)
+require('./core/routes/routes')(app)
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
