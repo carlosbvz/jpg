0fde90ed57e95aafc8ea3741afecd0418843af9f
Jose Pablo Granados <jpgcodecr@gmail.com>
2018-08-04 18:47:45 -0600

    Adding log handlebars helper

--- core/utils/handlebarsHelpers.js
+++ core/utils/handlebarsHelpers.js
@@ -1,1 +1,11 @@
-module.exports = {}
+/**
+ * @method log - Log a given parameter to the console
+ * @param {*} txt
+ */
+const log = (txt) => {
+    console.log(txt);
+};
+
+module.exports = {
+    log
+};
